dfde3356ebc638935f781011ed98d244f035cfe2
wetoszkp <zolnierzfortuny@gmail.com>
2016-06-28 19:10:32 +0200

    removed files from /src

---
+++ ga/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('ga')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
